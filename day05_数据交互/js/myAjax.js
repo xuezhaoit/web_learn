689b74c6d093e88cc5543e9d6c07d0c651a69cec
xuezhao <ghw947209263@163.com>
2020-02-26 09:12:10 +0800

    day05 上

--- day05_数据交互/js/myAjax.js
+++ day05_数据交互/js/myAjax.js
@@ -1,24 +1,57 @@
 function myAjax(options) {
-    options.type = options.type || ''
+    options.type = options.type || 'GET'
     options.data = options.data || {}
-    options.success = options.success || ''
-    options.error = options.error || ''
+    options.url = options.url || ''
     // 创建xmlrequest
     let xhr = new XMLHttpRequest()
-    // 开启连接
-    xhr.open('POST','2.php',true)
-    // 设置请求头
-    // xhr.setRequestHeader()
-    // 发送
-    xhr.send('a=1&b=2')
 
+    // 处理数据
+    // aa=1&bb=2
+    let arr = []
+    for (let key in options.data) {
+        arr.push(`${encodeURIComponent(key)}=${encodeURIComponent(options.data[key])}`)
+    }
+    let str_data = arr.join("&")
+    
+
+    // 开启连接
+    if (options.type == 'POST' ) {
+        xhr.open('POST', options.url, true)
+        // 设置请求头
+        xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded')
+        // 发送
+        xhr.send(str_data)
+    }else{
+        xhr.open('GET', options.url+'?'+str_data, true)
+        // 发送
+        xhr.send()
+    }
+    
     // 接受
     xhr.onreadystatechange=function (params) {
         if (xhr.readyState==4) {
-            if (xhr.status>200 && xhr.status<300 || xhr.status == 304) {
-                alert('成功：'+xhr.responseText)
+            console.log(xhr.status)
+            if (xhr.status>=200 && xhr.status<300 || xhr.status == 304) {
+                // 处理返回的报文
+                let data = xhr.responseText
+                switch (options.dataType) {
+                    case "json":
+                        // 兼容性处理
+                        if (window.JSON &&　JSON.parse　) {
+                            data = JSON.parse(data)
+                        } else {
+                            data = eval('('+str+')');
+                        }
+                        break;
+                    case 'xml':
+                        data = xhr.responseXML
+                        break;
+                }
+                alert('成功：'+data)
+                options.success && options.success(data)
             } else {
                 alert('失败')
+                options.error && options.error()
             }
         }
         
